33c31eade81e6cf882f6d636108eb3d6341f648b
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-02-27 13:33:25 +0530

    create increment and save button

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-let count = document.querySelector("#count-el")
-console.log(count)
+let countEl = document.querySelector("#count-el")
+
+let showEl = document.querySelector("#show-count")
+
+let count = 0
+
+document.querySelector("#increment-btn").addEventListener("click", function(e) {
+    count += 1
+    countEl.innerText = count
+    console.log(count)
+})
+
+document.querySelector("#save-btn").addEventListener("click", function(e) {
+    showEl.innerHTML += ` ${count} -`
+    countEl.innerHTML = 0
+    count = 0
+})
